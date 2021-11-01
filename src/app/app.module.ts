57f8d39ba88bc585336ecb0920a15170bb0a4704
Chung Nguyen <cnguyen@openwt.com>
2021-11-01 15:12:56 +0700

    add scrollable div inside scrollable div

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { UploadFileComponent } from './upload-file/upload-file.component';
 import { HoverCssThoughComponentComponent } from './hover-css-though-component/hover-css-though-component.component';
 import { InsideCssCompComponent } from './inside-css-comp/inside-css-comp.component';
+import { ScrollComponentComponent } from './scroll-component/scroll-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UploadFileComponent,
     HoverCssThoughComponentComponent,
-    InsideCssCompComponent
+    InsideCssCompComponent,
+    ScrollComponentComponent
   ],
   imports: [
     BrowserModule
